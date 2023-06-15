d7f491c459d34d3f7eb4b7bfe3de81dcebe06080
Saratonhaa339 <130287350+Saratonhaa339@users.noreply.github.com>
2023-06-15 15:10:35 +0300

    Add files via upload

---
+++ New-Sait-main/goods.js
@@ -0,0 +1,71 @@
+const GOODS = [{
+	id: 1,
+    name: 'MSI GeForce GTX 1650 D6 VENTUS',
+	price: 15600,
+	imgSrc: 'img/card1.jpg',
+}, {
+	id: 2,
+	name: 'Капибара',
+	price: 222,
+	imgSrc: 'goods/kapibara.png',
+}, {
+	id: 3,
+	name: 'Процессор Phenom II X4',
+	price: 333,
+	imgSrc: 'img/amd.jpg',
+}, {
+	id: 4,
+	name: 'Материнская плата GIGABYTE A320M-A PRO',
+	price: 444,
+	imgSrc: 'img/pl2.jpg',
+}, {
+	id: 5,
+	name: 'Ноутбук Maibenben M543',
+	price: 555,
+	imgSrc: 'img/nout1.jpg',
+}, {
+	id: 6,
+	name: 'Доктор Ливси',
+	price: 666,
+	imgSrc: 'goods/doctor livesey.png',
+}, {
+	id: 7,
+	name: 'машинка',
+	price: 1000000000,
+	imgSrc: 'goods/car.png',
+}, {
+	id: 8,
+    name: 'Шлёпа',
+	price: 1111,
+	imgSrc: 'goods/shlepa.png',
+}, {
+	id: 9,
+	name: 'Электро Тёрка',
+	price: 3500,
+	imgSrc: 'img/terka.jpg',
+}, {
+	id: 10,
+	name: 'Тяги',
+	price: 333,
+	imgSrc: 'goods/tyagi.png',
+}, {
+	id: 11,
+	name: 'Блинный Кот',
+	price: 444,
+	imgSrc: 'goods/blin.png',
+}, {
+	id: 12,
+	name: 'Смекалочка',
+	price: 555,
+	imgSrc: 'goods/smekalochka.png',
+}, {
+	id: 13,
+	name: 'Окно (Пластиковое)',
+	price: 13000,
+	imgSrc: 'img/ocno.jpg',
+}, {
+	id: 14,
+	name: 'Hogwarts Legacy',
+	price: 12000,
+	imgSrc: 'img/hovads.jpg',
+}];
---
+++ New-Sait-main/goods_2.js
@@ -0,0 +1,30 @@
+const GOODS_2 = [{ // меняем название массива
+	id: 1,
+    name: 'Личный кабинет',
+	imgSrc: 'img/zvezda.jpg',
+}, {
+	id: 2,
+	name: 'Капибара',
+	price: 100,
+	imgSrc: 'goods/kapibara.png',
+}, {
+	id: 3,
+	name: 'Видеокарты От',
+	price: 15000,
+	imgSrc: 'img/cardcatlog.jpg',
+}, {
+	id: 4,
+	name: 'Процессоры От',
+	price: 1500,
+	imgSrc: 'img/proth.jpg',
+}, {
+	id: 5,
+	name: 'Смекалочка',
+	price: 100,
+	imgSrc: 'goods/smekalochka.png',
+}, {
+	id: 6,
+	name: 'Доктор Ливси',
+	price: 100,
+	imgSrc: 'goods/doctor livesey.png',
+}];
